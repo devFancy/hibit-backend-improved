3043ee6c879c68e0fdc51b14f65ddad568b20a8a
devfancy <fancy.junyongmoon@gmail.com>
2024-01-05 19:13:42 +0900

    feat: 등록된 게시글을 모두 조회하는 API 개발

--- src/main/java/com/hibitbackendrefactor/post/presentation/PostController.java
+++ src/main/java/com/hibitbackendrefactor/post/presentation/PostController.java
@@ -5,16 +5,16 @@ import com.hibitbackendrefactor.auth.presentation.AuthenticationPrincipal;
 import com.hibitbackendrefactor.post.application.PostService;
 import com.hibitbackendrefactor.post.domain.Post;
 import com.hibitbackendrefactor.post.dto.request.PostCreateRequest;
+import com.hibitbackendrefactor.post.dto.response.PostsResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
@@ -25,6 +25,7 @@ import java.util.List;
 @Tag(name = "posts", description = "매칭 게시글")
 @RestController
 public class PostController {
+    private static final int PAGE_SIZE = 6;
 
     private final PostService postService;
 
@@ -33,7 +34,7 @@ public class PostController {
     }
 
     @PostMapping("/api/posts/new")
-    @Operation(summary = "/api/posts/new", description = "매칭 게시글 작성")
+    @Operation(summary = "/api/posts/new", description = "게시글을 등록한다.")
     public ResponseEntity<Post> save(@Parameter(hidden = true) @AuthenticationPrincipal final LoginMember loginMember
             , @Valid @RequestPart PostCreateRequest request
             , @RequestPart final List<MultipartFile> multipartFiles) throws IOException {
@@ -41,4 +42,10 @@ public class PostController {
         return ResponseEntity.created(URI.create("/posts/" + postId)).build();
     }
 
+    @GetMapping("api/posts")
+    @Operation(summary = "/api/posts", description = "등록된 게시글 전체를 조회한다.")
+    public ResponseEntity<PostsResponse> findAllProducts(@PageableDefault(size = PAGE_SIZE) Pageable pageable) {
+        PostsResponse responses = postService.findAllByPosts(pageable);
+        return ResponseEntity.ok().body(responses);
+    }
 }
