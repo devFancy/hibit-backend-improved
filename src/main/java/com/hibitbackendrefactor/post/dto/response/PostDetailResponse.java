05d11de15b6ce1cf87bae3663cbf8558e13d146a
devfancy <fancy.junyongmoon@gmail.com>
2024-01-06 12:18:20 +0900

    fix: 전시 관람인원과 함께하고 싶은 활동을 하나의 메서드로 정의, 매개변수에 이미지 리스트 추가
    - 작성자 이미지에 대한 코드는 추후에 작성할 예정

--- src/main/java/com/hibitbackendrefactor/post/dto/response/PostDetailResponse.java
+++ src/main/java/com/hibitbackendrefactor/post/dto/response/PostDetailResponse.java
@@ -8,59 +8,57 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+import static com.hibitbackendrefactor.post.dto.response.PostResponse.AttendanceAndTogetherActivity;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostDetailResponse {
     private Long id;
-    private String writerName;
     private Long writerId;
+    private String writerName;
     private String writerImage;
     private String title;
     private String content;
     private String exhibition;
-    private int exhibitionAttendance;
+    private List<String> exhibitionAttendanceAndTogetherActivity;
     private List<PostPossibleTime> possibleTimes;
     private String openChatUrl;
-    private TogetherActivity togetherActivity;
     private PostStatus postStatus;
-    private List<PostImage> postImage;
+    private List<String> postImages;
 
     @Builder
-    public PostDetailResponse(final Long id, final String writerName, final Long writerId, String writerImage
+    public PostDetailResponse(final Long id, final Long writerId, final String writerName, String writerImage
             , final String title, final String content, final String exhibition
-            , final int exhibitionAttendance, final List<PostPossibleTime> possibleTimes
-            , final String openChatUrl, final TogetherActivity togetherActivity
-            , final PostStatus postStatus, final List<PostImage> postImage) {
+            , final List<String> exhibitionAttendanceAndTogetherActivity, final List<PostPossibleTime> possibleTimes
+            , final String openChatUrl, final PostStatus postStatus, final List<String> postImages) {
         this.id = id;
-        this.writerName = writerName;
         this.writerId = writerId;
-        this.writerImage = writerImage;
+        this.writerName = writerName;
+//        this.writerImage = writerImage;
         this.title = title;
         this.content = content;
         this.exhibition = exhibition;
-        this.exhibitionAttendance = exhibitionAttendance;
+        this.exhibitionAttendanceAndTogetherActivity = exhibitionAttendanceAndTogetherActivity;
         this.possibleTimes = possibleTimes;
         this.openChatUrl = openChatUrl;
-        this.togetherActivity = togetherActivity;
         this.postStatus = postStatus;
-        this.postImage = postImage;
+        this.postImages = postImages;
     }
 
-    public static PostDetailResponse of(final Post post) {
+    public static PostDetailResponse of(final Post post, List<String> imageUrls) {
         return PostDetailResponse.builder()
                 .id(post.getId())
-                .writerName(post.getMember().getDisplayName())
                 .id(post.getMember().getId())
+                .writerName(post.getMember().getDisplayName())
                 // 게시글 작성자 이미지 추가해야함
                 .title(post.getTitle())
                 .content(post.getContent())
                 .exhibition(post.getExhibition())
-                .exhibitionAttendance(post.getExhibitionAttendance())
+                .exhibitionAttendanceAndTogetherActivity(AttendanceAndTogetherActivity(post.getExhibitionAttendance(), post.getTogetherActivity()))
                 .possibleTimes(post.getPossibleTimes())
                 .openChatUrl(post.getOpenChatUrl())
-                .togetherActivity(post.getTogetherActivity())
                 .postStatus(post.getPostStatus())
-                .postImage(post.getPostImages())
+                .postImages(imageUrls)
                 .build();
     }
 }
