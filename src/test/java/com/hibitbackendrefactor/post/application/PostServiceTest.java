fe93a06957dbf91eb6b9409cc11d48d3652bdbdb
devfancy <fancy.junyongmoon@gmail.com>
2024-01-10 22:19:45 +0900

    test: Business Layer 에 대한 테스트 코드 작성

---
+++ src/test/java/com/hibitbackendrefactor/post/application/PostServiceTest.java
@@ -0,0 +1,74 @@
+package com.hibitbackendrefactor.post.application;
+
+import com.hibitbackendrefactor.member.domain.Member;
+import com.hibitbackendrefactor.member.domain.MemberRepository;
+import com.hibitbackendrefactor.post.domain.Post;
+import com.hibitbackendrefactor.post.domain.PostRepository;
+import com.hibitbackendrefactor.post.dto.request.PostCreateRequest;
+import com.hibitbackendrefactor.profile.domain.Profile;
+import com.hibitbackendrefactor.profile.domain.ProfileRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static com.hibitbackendrefactor.common.fixtures.MemberFixtures.팬시;
+import static com.hibitbackendrefactor.common.fixtures.PostFixtures.*;
+import static com.hibitbackendrefactor.common.fixtures.ProfileFixtures.팬시_프로필;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class PostServiceTest {
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private PostService postService;
+
+    @AfterEach
+    void tearDown() {
+        postRepository.deleteAll();
+        profileRepository.deleteAll();
+        memberRepository.deleteAll(); // deleteAllInBatch()인 경우 오류는 나중에 해결할 예정(23.01.10)
+    }
+
+    @DisplayName("게시글을 등록한다.")
+    @Test
+    void 게시글을_등록한다() {
+        // given
+        Member 팬시 = 팬시();
+        Profile 팬시_프로필 = 팬시_프로필(팬시);
+        profileRepository.save(팬시_프로필);
+
+        PostCreateRequest request = PostCreateRequest.builder()
+                .title(게시글제목1)
+                .content(게시글내용1)
+                .exhibition(전시회제목1)
+                .exhibitionAttendance(전시관람인원1)
+                .possibleTimes(전시관람희망날짜1)
+                .openChatUrl(오픈채팅방Url1)
+                .togetherActivity(함께하고싶은활동1)
+                .imageName(게시글이미지1)
+                .postStatus(모집상태1)
+                .build();
+
+        // when
+        Long newPostId = postService.save(팬시_프로필.getMember().getId(), request);
+        Post savedPost = postRepository.findByMemberId(팬시.getId()).orElse(null);
+
+        // then
+        assertThat(newPostId).isNotNull();
+        assertEquals("프로젝트_해시테크", savedPost.getTitle());
+
+    }
+}
