f9e07e9ea1caeccb73055ee9ac26a88400784dc7
devfancy <fancy.junyongmoon@gmail.com>
2024-01-17 21:27:43 +0900

    test: 회원의 닉네임에 대한 예외 케이스 추가

--- src/test/java/com/hibitbackendrefactor/member/domain/MemberTest.java
+++ src/test/java/com/hibitbackendrefactor/member/domain/MemberTest.java
@@ -30,13 +30,23 @@ class MemberTest {
             .isInstanceOf(InvalidMemberException.class);
     }
 
-    @DisplayName("회원의 닉네임 형식이 맞지 않으면 예외가 발생한다.")
+    @DisplayName("회원의 닉네임 형식이 빈칸이거나 공백이면 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"", " ", "일이삼사오육칠팔구십" + "일이삼사오육칠팔구십" + "일"})
-    void 회원의_닉네임_형식이_맞지_않으면_예외가_발생한다(final String displayName) {
+    @ValueSource(strings = {"", " "})
+    void 회원의_닉네임_형식이_빈칸이거나_공백이면_예외가_발생한다(final String displayName) {
 
         // given & when & then
         assertThatThrownBy(() -> new Member(팬시_이메일, displayName, SocialType.GOOGLE))
                 .isInstanceOf(InvalidMemberException.class);
     }
+
+    @DisplayName("회원의 닉네임 글자가 20자 초과하면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"일이삼사오육칠팔구십일이삼사오육칠팔구십일", "일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십"})
+    void 회원의_닉네임_글자가_20자_초과하면_예외가_발생한다(final String displayName) {
+
+        // given & when & then
+        assertThatThrownBy(() -> new Member(팬시_이메일, displayName, SocialType.GOOGLE))
+                .isInstanceOf(InvalidMemberException.class);
+     }
 }
