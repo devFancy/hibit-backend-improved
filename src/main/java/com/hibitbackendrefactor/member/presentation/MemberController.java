8859f3cda7526e3ea40afdd15faffe868bf43184
devfancy <fancy.junyongmoon@gmail.com>
2024-01-19 16:57:44 +0900

    chore: 불필요한 클래스와 메서드 제거

--- src/main/java/com/hibitbackendrefactor/member/presentation/MemberController.java
+++ src/main/java/com/hibitbackendrefactor/member/presentation/MemberController.java
@@ -3,10 +3,7 @@ package com.hibitbackendrefactor.member.presentation;
 import com.hibitbackendrefactor.auth.dto.LoginMember;
 import com.hibitbackendrefactor.auth.presentation.AuthenticationPrincipal;
 import com.hibitbackendrefactor.member.application.MemberService;
-import com.hibitbackendrefactor.member.domain.Member;
 import com.hibitbackendrefactor.member.domain.MemberRepository;
-import com.hibitbackendrefactor.member.exception.NotFoundMemberException;
-import com.hibitbackendrefactor.member.dto.MemberIsProfileResponse;
 import com.hibitbackendrefactor.member.dto.MemberResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,15 +29,6 @@ public class MemberController {
     public ResponseEntity<MemberResponse> findMe(@AuthenticationPrincipal final LoginMember loginMember) {
         MemberResponse response = memberService.findById(loginMember.getId());
         return ResponseEntity.ok(response);
-    }
-
-    @GetMapping("/find")
-    public ResponseEntity<MemberIsProfileResponse> findIdx(@AuthenticationPrincipal final LoginMember loginMember) {
-        Member member = memberRepository.findById(loginMember.getId())
-                .orElseThrow(NotFoundMemberException::new);
-
-        MemberIsProfileResponse memberIsProfileResponse = new MemberIsProfileResponse(member);
-        return ResponseEntity.ok(memberIsProfileResponse);
     }
 
 }
