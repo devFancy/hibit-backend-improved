66a64e4f8221bbd24e28a6c637c50d77ce7f1cbc
devfancy <fancy.junyongmoon@gmail.com>
2024-01-19 23:21:53 +0900

    test: PostRepository, Post에 대한 테스트 코드 추가

--- src/test/java/com/hibitbackendrefactor/post/domain/PostRepositoryTest.java
+++ src/test/java/com/hibitbackendrefactor/post/domain/PostRepositoryTest.java
@@ -2,67 +2,75 @@ package com.hibitbackendrefactor.post.domain;
 
 import com.hibitbackendrefactor.IntegrationTestSupport;
 import com.hibitbackendrefactor.member.domain.Member;
+import com.hibitbackendrefactor.member.domain.MemberRepository;
+import com.hibitbackendrefactor.profile.domain.Profile;
+import com.hibitbackendrefactor.profile.domain.ProfileRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Optional;
 
-import static com.hibitbackendrefactor.common.fixtures.MemberFixtures.브루스;
 import static com.hibitbackendrefactor.common.fixtures.MemberFixtures.팬시;
-import static com.hibitbackendrefactor.common.fixtures.PostFixtures.*;
-import static org.assertj.core.api.AssertionsForClassTypes.tuple;
+import static com.hibitbackendrefactor.common.fixtures.PostFixtures.프로젝트_해시테크;
+import static com.hibitbackendrefactor.common.fixtures.ProfileFixtures.팬시_프로필;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
 @Transactional
 class PostRepositoryTest extends IntegrationTestSupport {
 
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private ProfileRepository profileRepository;
 
     @Autowired
     private PostRepository postRepository;
 
-    @DisplayName("등록된 모든 게시글을 조회한다.")
-    @Test
-    void 등록된_모든_게시글을_조회한다() {
-        // given
-        Post post1 = createPost(팬시(), 게시글제목1, 게시글내용1, 전시회제목1, 전시관람인원1, 전시관람희망날짜1, 오픈채팅방Url1, 함께하고싶은활동1, 게시글이미지1, 모집상태1);
-        Post post2 = createPost(브루스(), 게시글제목2, 게시글내용2, 전시회제목2, 전시관람인원2, 전시관람희망날짜2, 오픈채팅방Url2, 함께하고싶은활동2, 게시글이미지2, 모집상태2);
+    private Member member;
 
-        postRepository.saveAll(List.of(post1, post2));
+    private Profile profile;
+
+    private Post post;
+
+    @BeforeEach
+    void setUp() {
+        member = 팬시();
+        memberRepository.save(member);
+        profile = 팬시_프로필(member);
+        profileRepository.save(profile);
+        post = 프로젝트_해시테크(member);
+        postRepository.save(post);
+    }
+
+    @DisplayName("게시글과 회원 테이블이 정상적으로 매핑이 된다.")
+    @Test
+    void 게시글과_회원_테이블이_정상적으로_매핑이_된다() {
+        // given
+        Post foundPost = postRepository.getById(post.getId());
+
+        // when & then
+        Assertions.assertThat(foundPost.getMember().getId()).isNotNull();
+    }
+
+    @DisplayName("특정 회원 ID에 해당하는 게시글을 찾는다.")
+    @Test
+    void 특정_회원_ID에_해당하는_게시글을_찾는다() {
+        // given
+        Long memberId = post.getMember().getId();
 
         // when
-        List<Post> posts = postRepository.findAllByOrderByCreatedDateTimeDesc();
+        Optional<Post> actual = postRepository.findByMemberId(memberId);
 
         // then
-        Assertions.assertThat(posts).hasSize(2)
-                .extracting("title", "content", "openChatUrl")
-                .containsExactlyInAnyOrder(
-                        tuple("프로젝트_해시테크", "프로젝트 해시 태크(http://projecthashtag.net/) 보러가실 분 있으면 아래 댓글 남겨주세요~", "http://projecthashtag.net/"),
-                        tuple("오스틴리 전시회", "오스틴리 전시회 보고, 같이 담소하게 얘기 나누실 분 있으시면 아래 댓글 남겨주세요~", "http://ostin.net/")
-                );
-     }
-
-
-     private Post createPost(Member member, String title, String content,
-                             String exhibition, int exhibitionAttendance, LocalDateTime postPossibleTime,
-                             String openChatUrl, TogetherActivity togetherActivity, String imageName, PostStatus postStatus) {
-
-        Post post1 = Post.builder()
-                .member(member)
-                .title(title)
-                .content(content)
-                .exhibition(exhibition)
-                .exhibitionAttendance(exhibitionAttendance)
-                .possibleTime(postPossibleTime)
-                .openChatUrl(openChatUrl)
-                .togetherActivity(togetherActivity)
-                .imageName(imageName)
-                .postStatus(postStatus)
-                .build();
-        return post1;
-     }
+        assertThat(actual).isPresent();
+        Post foundPost = actual.get();
+        assertThat(foundPost.getMember().getId()).isEqualTo(memberId);
+    }
 }
