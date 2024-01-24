dabe630922e80efc498d9c5526472e5628807e8a
devfancy <fancy.junyongmoon@gmail.com>
2024-01-24 21:43:32 +0900

    feat: 에러를 요청하고 반환하는 dto 생성

---
+++ src/main/java/com/hibitbackendrefactor/global/error/dto/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.hibitbackendrefactor.global.error.dto;
+
+public class ErrorResponse {
+    private final String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
