b0893f2fac37e705f1387bee53f89fb25daf716a
devfancy <fancy.junyongmoon@gmail.com>
2024-02-01 19:52:12 +0900

    test: PostRepository에 대한 테스트 코드 작성

--- src/test/java/com/hibitbackendrefactor/post/domain/PostRepositoryTest.java
+++ src/test/java/com/hibitbackendrefactor/post/domain/PostRepositoryTest.java
@@ -11,15 +11,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
 import java.util.Optional;
 
 import static com.hibitbackendrefactor.common.fixtures.MemberFixtures.팬시;
-import static com.hibitbackendrefactor.common.fixtures.PostFixtures.프로젝트_해시테크;
+import static com.hibitbackendrefactor.common.fixtures.PostFixtures.*;
 import static com.hibitbackendrefactor.common.fixtures.ProfileFixtures.팬시_프로필;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
+import static org.springframework.data.domain.Sort.Direction.DESC;
 
 @ActiveProfiles("test")
 @Transactional
@@ -34,11 +42,16 @@ class PostRepositoryTest extends IntegrationTestSupport {
     @Autowired
     private PostRepository postRepository;
 
+    @PersistenceContext
+    private EntityManager em;
+
     private Member member;
 
     private Profile profile;
 
-    private Post post;
+    private Post post0;
+    private Post post1;
+    private Post post2;
 
     @BeforeEach
     void setUp() {
@@ -46,15 +59,17 @@ class PostRepositoryTest extends IntegrationTestSupport {
         memberRepository.save(member);
         profile = 팬시_프로필(member);
         profileRepository.save(profile);
-        post = 프로젝트_해시테크(member);
-        postRepository.save(post);
+        post0 = 오스틴리_전시회(member);
+        post1 = 프로젝트_해시테크(member);
+        post2 = 프로젝트_해시테크_2(member);
+        postRepository.saveAll(List.of(post0, post1, post2));
     }
 
     @DisplayName("게시글과 회원 테이블이 정상적으로 매핑이 된다.")
     @Test
     void 게시글과_회원_테이블이_정상적으로_매핑이_된다() {
         // given
-        Post foundPost = postRepository.findById(post.getId())
+        Post foundPost = postRepository.findById(post1.getId())
                 .orElseThrow(NotFoundPostException::new);
 
         // when & then
@@ -65,7 +80,7 @@ class PostRepositoryTest extends IntegrationTestSupport {
     @Test
     void 특정_회원_ID에_해당하는_게시글을_찾는다() {
         // given
-        Long memberId = post.getMember().getId();
+        Long memberId = post1.getMember().getId();
 
         // when
         Optional<Post> actual = postRepository.findByMemberId(memberId);
@@ -74,5 +89,25 @@ class PostRepositoryTest extends IntegrationTestSupport {
         assertThat(actual).isPresent();
         Post foundPost = actual.get();
         assertThat(foundPost.getMember().getId()).isEqualTo(memberId);
+    }
+    @DisplayName("특정 쿼리에 부합하는 글의 개수를 가져온다.")
+    @Test
+    void findPostPagesByQuery() {
+        // given
+        Page<Post> result = postRepository.findPostPagesByQuery(PageRequest.of(0, 3, DESC, "created_date_time"), "");
+
+        // when & then
+        assertThat(result.getTotalElements()).isEqualTo(3L);
+    }
+
+    @DisplayName("특정 쿼리에 부합하는 게시글을 최신순으로 가져온다.")
+    @Test
+    void findPostSlicePageByQuery() {
+        // given
+        Slice<Post> result = postRepository.findPostSlicePageByQuery(PageRequest.of(0, 2, DESC, "created_date_time"), "");
+
+        // when & then
+        assertThat(result.getContent()).containsExactly(post2, post1);
+        assertThat(result.isLast()).isEqualTo(false);
     }
 }
