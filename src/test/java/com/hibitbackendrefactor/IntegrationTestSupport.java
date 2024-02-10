88f3bd8987b91486b75d046efbc960cb24902df8
devfancy <fancy.junyongmoon@gmail.com>
2024-02-10 15:49:28 +0900

    test: IntegrationTestSupport 추상화 클래스에 setUp 메서드 추가
    - 각 테스트 실행 전 데이터베이스 초기화 적용

--- src/test/java/com/hibitbackendrefactor/IntegrationTestSupport.java
+++ src/test/java/com/hibitbackendrefactor/IntegrationTestSupport.java
@@ -1,8 +1,12 @@
 package com.hibitbackendrefactor;
 
+import com.hibitbackendrefactor.auth.domain.TokenRepository;
 import com.hibitbackendrefactor.auth.dto.LoginMember;
+import com.hibitbackendrefactor.common.DatabaseCleaner;
 import com.hibitbackendrefactor.config.ExternalApiConfig;
 import com.hibitbackendrefactor.global.config.JpaAuditingConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
@@ -14,4 +18,16 @@ import static com.hibitbackendrefactor.common.fixtures.MemberFixtures.FANCY_ID;
 @SpringBootTest(classes = ExternalApiConfig.class)
 public abstract class IntegrationTestSupport {
     protected static final LoginMember LOGIN_MEMBER = new LoginMember(FANCY_ID);
+
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+    @Autowired
+    private TokenRepository tokenRepository;
+
+    @BeforeEach
+    void setUp() {
+        databaseCleaner.execute();
+        tokenRepository.deleteAll();
+    }
 }
