8387c1ccf00006e219483da94dc08b5b13e2f767
devfancy <fancy.junyongmoon@gmail.com>
2024-02-21 01:54:55 +0900

    build: spring rest docs 의존성 추가
    - aws s3 의존성 제거

--- src/main/java/com/hibitbackendrefactor/HibitBackendRefactorApplication.java
+++ src/main/java/com/hibitbackendrefactor/HibitBackendRefactorApplication.java
@@ -3,13 +3,7 @@ package com.hibitbackendrefactor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-        exclude = {
-                org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
-                org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
-                org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
-        }
-)
+@SpringBootApplication
 public class HibitBackendRefactorApplication {
 
     public static void main(String[] args) {
